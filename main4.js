d49ffcb16bcc2235e01733577e6f1c880fa9a661
beccanaul <beccanaul@gmail.com>
2016-10-19 21:31:43 -0700

    adds comments re: chrome extension

--- main4.js
+++ main4.js
@@ -1,13 +1,22 @@
+// using chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en
+// to bypass cors error
+
 document.addEventListener("DOMContentLoaded", function(_event){
   var url = 'https://api.tumblr.com/v2/blog/p5js.tumblr.com/posts/photo?api_key=oTugrABVFxS2grXbeYvjv1MAX0BrNByCKqeG6q7yXc5X7jtGOc';
   document.querySelector('#js-stuff').textContent = 'hello';
 
   var button = document.querySelector('#js-click');
   button.addEventListener('click', function(event){
+    // fetches tumblr
+
     fetch(url).then(function(response){
       return response.json();
+      console.log(response.json());
     }).then(function(myJson){
-      document.querySelector('#photo').src = (myJson.response.posts[1].photos["0"].original_size.url)
+      // logs data from api
+      console.log(myJson);
+      // adds photo to img in html
+      document.querySelector('img').src = (myJson.response.posts[0].photos["0"].original_size.url);
     });
   });
 });
